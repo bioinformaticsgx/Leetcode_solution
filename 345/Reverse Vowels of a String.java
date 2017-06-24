8af9afe0b20fc605b4a284d433a62f810ccea626
Jerry Xu <Guang.Xu@umassmed.edu>
2017-06-24 12:57:17 -0400

    Update Reverse Vowels of a String.java

--- 345/Reverse Vowels of a String.java
+++ 345/Reverse Vowels of a String.java
@@ -1,4 +1,13 @@
-while(start<end){
+
+// Best solution using two pointers
+public class Solution {
+public String reverseVowels(String s) {
+    if(s == null || s.length()==0) return s;
+    String vowels = "aeiouAEIOU";
+    char[] chars = s.toCharArray();
+    int start = 0;
+    int end = s.length()-1;
+    while(start<end){
         
         while(start<end && !vowels.contains(chars[start]+"")){
             start++;
@@ -7,3 +16,44 @@ while(start<end){
         while(start<end && !vowels.contains(chars[end]+"")){
             end--;
         }
+        
+        char temp = chars[start];
+        chars[start] = chars[end];
+        chars[end] = temp;
+        
+        start++;
+        end--;
+    }
+    return new String(chars);
+}
+        
+        
+        
+// My solutions:
+
+        public class Solution {
+    public String reverseVowels(String s) {
+        
+        List<Integer> listpos = new ArrayList<Integer>();
+        List<Character> listvow = new ArrayList<Character>();
+        
+        char[] sarray = s.toCharArray();
+        
+        for (int i=0; i<s.length();i++) {
+             if("aeiouAEIOU".indexOf(s.charAt(i))!=-1) {
+                 listpos.add(i);
+                 listvow.add(s.charAt(i));
+             }
+        }
+        
+        Collections.reverse(listvow);
+        // System.out.println(listpos);
+        // System.out.println(listvow);
+        
+        for (int j=0; j<listpos.size();j++) {
+            sarray[listpos.get(j)] = listvow.get(j);
+        }
+        
+        return new String(sarray);
+    }
+}
